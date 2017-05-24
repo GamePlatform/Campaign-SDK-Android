3cc01f502ae4a82a451ce02b879b1a9a2a8f3ec2
vi01et03 <kairos9603@gmail.com>
2017-05-25 02:11:44 +0900

    Add image web view

--- app/src/main/java/com/advisor/campaign/MainActivity.kt
+++ app/src/main/java/com/advisor/campaign/MainActivity.kt
@@ -2,7 +2,9 @@ package com.advisor.campaign
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import com.advisor.campaign.campaignsdk.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        popup.settings.javaScriptEnabled = true
+        popup.loadUrl("file:///android_asset/popup.html")
 
     }
 }
