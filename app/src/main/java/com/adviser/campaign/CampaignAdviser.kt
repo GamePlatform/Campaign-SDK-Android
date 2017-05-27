d6a50d7eb05b4c2ee2318babf1e8a4a244471166
vi01et03 <kairos9603@gmail.com>
2017-05-27 21:12:29 +0900

    change Javascript Interface name

--- app/src/main/java/com/adviser/campaign/CampaignAdviser.kt
+++ app/src/main/java/com/adviser/campaign/CampaignAdviser.kt
@@ -6,16 +6,15 @@ import android.webkit.WebView
  * Created by Kairos on 2017. 5. 25..
  */
 
+// Campaign Adviser
 class CampaignAdviser{
 
     fun loadCampaign(popup: WebView, locationId: Int) {
 
-        // enable javascript in webview
         popup.settings.javaScriptEnabled = true
-        popup.addJavascriptInterface(WebAppInterface(HttpRequestAgent(1)), "android")
+        popup.addJavascriptInterface(WebAppInterface(HttpRequestAgent(1)), "campaign")
         popup.loadUrl("file:///android_asset/popup.html")
 
     }
-
 
 }
