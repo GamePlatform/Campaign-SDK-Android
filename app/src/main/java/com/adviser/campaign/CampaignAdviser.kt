5a1f977012a94898d5de2e948fb2e22969df3343
vi01et03 <kairos9603@gmail.com>
2017-06-02 18:42:36 +0900

    chagne loadCampaign and seperate CampaignInfo from HttpRequestAgent

--- app/src/main/java/com/adviser/campaign/CampaignAdviser.kt
+++ app/src/main/java/com/adviser/campaign/CampaignAdviser.kt
@@ -2,7 +2,6 @@ package com.adviser.campaign
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
 import android.webkit.WebView
 
 /**
@@ -12,30 +11,40 @@ import android.webkit.WebView
 // Campaign Adviser
 class CampaignAdviser{
 
-//    fun loadCampaign(popup: WebView, locationId: Int) {
-//
-//        popup.settings.javaScriptEnabled = true
-//        val agent: HttpRequestAgent = HttpRequestAgent(1)
-//        agent.reqParser()
-//        popup.addJavascriptInterface(agent, "campaign")
-//        popup.loadUrl("file:///android_asset/popup.html")
-//    }
+    var alertList = mutableListOf<AlertDialog?>()
 
-    fun loadDynamicCampaign(context: Context, locationId: Int) {
+    fun showCampaignAllInOnce() {
+        //TODO
+    }
+
+    fun showCampaignOneByOne() {
+        //TODO
+    }
+
+    // Load all Campaign in locationId
+    fun loadCampaign(context: Context, locationId: Int) {
+        val agent: HttpRequestAgent = HttpRequestAgent(locationId)
+        val ci = agent.reqParser()
+
+//        while (!ci.isDone()) {
+//            alertList.add(makeSingleCampaignDialog(context, ci))
+//        }
+        makeSingleCampaignDialog(context, ci)!!.show()
+    }
+
+    // campaign make dialog
+    fun makeSingleCampaignDialog(context: Context, campaignsInfo: CampaignsInfo): AlertDialog? {
         // dialog setting
-//        var builder = AlertDialog.Builder(context)
-//        builder.setNegativeButton("닫기", DialogInterface.OnClickListener { dialogInterface, i ->  }).show()
         val alert = AlertDialog.Builder(context).create()
 
         // popup(webview) setting
         val popup = WebView(context)
-        val agent: HttpRequestAgent = HttpRequestAgent(locationId)
-        agent.reqParser()
         popup.settings.javaScriptEnabled = true
-        popup.addJavascriptInterface(WebAppInterface(agent, alert), "campaign")
+        popup.addJavascriptInterface(WebAppInterface(campaignsInfo, alert), "campaign")
         popup.loadUrl("file:///android_asset/popup.html")
 
         alert.setView(popup)
-        alert.show()
+        //alert.show()
+        return alert
     }
 }
--- app/src/main/java/com/adviser/campaign/HttpRequestAgent.kt
+++ app/src/main/java/com/adviser/campaign/HttpRequestAgent.kt
@@ -1,5 +1,6 @@
 package com.adviser.campaign
 
+import android.util.Log
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.HttpURLConnection
@@ -9,18 +10,17 @@ import kotlin.concurrent.thread
 /**
  * Created by Kairos on 2017. 5. 26..
  */
+// TODO Change Singletone and Static
 class HttpRequestAgent {
 
     val reqRootUrl = "http://211.253.28.194:30022/api/campaign/locations/"  //TODO load from config file
     val locationId : Int
-    var urls: Array<String>? = null
-    var cur: Int = -1
 
     constructor(locationId : Int) {
         this.locationId = locationId
     }
 
-    fun reqParser() {
+    fun reqParser(): CampaignsInfo {
         val urls: Array<String>?
 
         val images: JSONArray = JSONObject(GET(reqRootUrl + locationId)).getJSONArray("images")
@@ -30,13 +30,17 @@ class HttpRequestAgent {
             urls[i] = (images[i] as JSONObject).getString("url")
         }
 
-        this.urls = urls
-        cur = 0
+        Log.d("HttpRequestAgent", "reqParser/loadURLs Complete")
+        for(url in urls) {
+            Log.v("HttpRequestAgent", "reqParser/loadUrls Image URL: " + url)
+        }
+        return CampaignsInfo(urls)
     }
 
     // HTTP GET request using URL string
     // return request body
     private fun GET(url : String?) : String?{
+        Log.d("HttpRequestAgent", "GET/Request Url Start URL: " + url)
         var result: String = ""
 
         // Null String check
@@ -53,6 +57,7 @@ class HttpRequestAgent {
                 }
             }).join()
         }
+        Log.d("HttpRequestAgent", "GET/Request URL Done URL: " + url)
         return result.toString()
     }
 }
