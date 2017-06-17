bf20daa829079bd463eec64ff8c9b66f3eb0eea0
EunSeop Shin <kairos9603@gmail.com>
2017-06-17 16:33:51 +0900

    Revert "Refactoring "

--- app/src/test/kotlin/com/adviser/campaign/HttpRequestAgentTest.kt
+++ app/src/test/kotlin/com/adviser/campaign/HttpRequestAgentTest.kt
@@ -1,6 +1,5 @@
 package com.adviser.campaign
 
-import com.adviser.campaign.model.HttpRequestAgent
 import org.junit.Assert.assertEquals
 import org.testng.annotations.Test
 
@@ -10,10 +9,11 @@ import org.testng.annotations.Test
  */
 class HttpRequestAgentTest {
 
-  @Test
-  fun constructorTest() {
-    val hra: HttpRequestAgent = HttpRequestAgent(1)
-    assertEquals(hra.reqRootUrl, "http://211.253.28.194:30022/api/campaign/locations/")
-    assertEquals(hra.locationId, 1)
-  }
+    @Test
+    fun constructorTest() {
+        val hra: HttpRequestAgent = HttpRequestAgent(1)
+        assertEquals(hra.reqRootUrl, "http://211.253.28.194:30022/api/campaign/locations/")
+        assertEquals(hra.locationId, 1)
+        assertEquals(hra.cur, 0)
+    }
 }
